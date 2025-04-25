679316de7ad0e9a26b94b0f1ff27b1c42365f489
mohamedrahili <mohamedrahili136@example.com>
2025-04-25 10:51:44 +0100

    Initial commit

---
+++ lib/common_widget/image_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../common/color_extension.dart';
+
+class ImageButton extends StatelessWidget {
+  final String image;
+  final VoidCallback onPressed;
+  const ImageButton({super.key, required this.image, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(16),
+      onTap: onPressed,
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          border: Border.all(
+            // ignore: deprecated_member_use
+            color: TColor.border.withOpacity(0.15),
+          ),
+          // ignore: deprecated_member_use
+          color: TColor.gray60.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        alignment: Alignment.center,
+        child: Image.asset(image, width: 50, height: 50, color: TColor.gray70),
+      ),
+    );
+  }
+}
---
+++ lib/common_widget/status_button.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../common/color_extension.dart';
+
+class StatusButton extends StatelessWidget {
+  final String title;
+  final String value;
+  final Color statusColor;
+
+  final VoidCallback onPressed;
+  const StatusButton(
+      {super.key,
+      required this.title,
+      required this.value,
+      required this.statusColor,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: 68,
+            decoration: BoxDecoration(
+              border: Border.all(
+                // ignore: deprecated_member_use
+                color: TColor.border.withOpacity(0.15),
+              ),
+              // ignore: deprecated_member_use
+              color: TColor.gray60.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: TColor.gray40,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  value,
+                  style: TextStyle(
+                      color: TColor.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 60,
+            height: 1,
+            color: statusColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
